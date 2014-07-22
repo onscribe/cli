ff794523733952e032b7a01cc3dfff1e272f079b
Makis Tracend <makis.tracend@gmail.com>
2014-07-21 17:43:17 -0700

    + examples folder

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the Onscribe CLI
+
+# 1. Install the main executable
+npm install -g onscribe-cli
+
+#2. Setup onscribe (only needed once)
+onscribe setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+onscribe -h
